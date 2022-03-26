eb22ecb3a3f6d5558e68de40eeecc566160db921
asloth <sbenelramirez@gmail.com>
2022-03-25 21:00:34 -0500

    fix in emotion endpoint

--- apis/model.js
+++ apis/model.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export async function emotionRecognition(text) {
+
   return await axios
     .post("http://68.183.124.29/distilbert", {
       data: {
@@ -8,7 +9,8 @@ export async function emotionRecognition(text) {
       },
     })
     .then((response) => {
-      return response.data;
+      let res = response.data.slice(1, response.data.length-1);
+      return JSON.parse(res);
     })
     .catch((error) => {
       console.log(error);
