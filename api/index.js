4a57cc198a5134cf54d9ee66efc2d6aaa36d336a
asloth <sbenelramirez@gmail.com>
2022-05-12 00:21:52 -0500

    fix: changing routes names

--- api/index.js
+++ api/index.js
@@ -18,21 +18,21 @@ app.use(express.urlencoded({extended:true}));
 app.use(Cors());
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
   res.end('<h1>Web utilizada para alojar servicios de la aplicacion movil Emcare</h1>');
 })
 
-app.get('/user-compromise', (req, res) => {
+app.get('/api/user-compromise', (req, res) => {
   res.sendFile(path.resolve(__dirname, "views/user-compromise.html"));
 })
 
-app.get('/privacy-policy', (req, res) => {
+app.get('/api/privacy-policy', (req, res) => {
   res.sendFile(path.resolve(__dirname, "views/privacy-policy.html"));
 })
 //Ruta para reconocimmiento de emociones
-app.post('/ibm', async (req, res) => {
+app.post('/api/ibm', async (req, res) => {
    let text = req.body.message;
    let response = await detect_intent_text(text);
    let nregs = await saveSentiment( req.body.userid, response);
@@ -42,46 +42,46 @@ app.post('/ibm', async (req, res) => {
    res.json(response);
 })
 //Ruta para reconocimmiento de emociones
-app.post('/emotion', async (req, res) => {
+app.post('/api/emotion', async (req, res) => {
   let text = req.body.message;
    let response = await emotionRecognition(text);
    let nregs = await saveSentiment( req.body.userid, response);
    res.json(response);
 })
 //Ruta para comunicación con dialogflow
-app.post('/dialog', async (req, res) => {
+app.post('/api/dialog', async (req, res) => {
     const response = await detectIntent(req.body.message);
     res.json(response);
 })
 //Ruta para obtener emociones por usuario
-app.post('/get-sentiment', async (req, res) => {
+app.post('/api/get-sentiment', async (req, res) => {
   const response = await getSentiment(req.body.userid);
   res.json(response);
 })
 //Ruta para agregar un usuario
-app.post('/new-user', async (req, res) => {
+app.post('/api/new-user', async (req, res) => {
   const response = await setNewUser(req.body.userid, req.body.username);
   res.json(response);
 })
 //Ruta para obtener la tendencia de las emociones
-app.post('/get-analysis', async (req, res) => {
+app.post('/api/get-analysis', async (req, res) => {
   const response = await getDataAnalysis(req.body.userid);
   res.json(response)
 })
 //Ruta para listar a todos los usuarios
-app.post('/users', async (req, res) => {
+app.post('/api/users', async (req, res) => {
   const response = await getAllUsers();
   res.json(response);
 })
 
 // Login
-app.post("/login", async (req, res) => {
+app.post("/api/login", async (req, res) => {
   const response = await login(req.body.username, req.body.password);
   res.json(response);
   });
 
   //Resgistrar un psicologo
-app.post("/register", async (req, res) => {
+app.post("/api/register", async (req, res) => {
   const response = await setNewPsicologist(req.body.username, req.body.password);
   res.json(response);
   });
